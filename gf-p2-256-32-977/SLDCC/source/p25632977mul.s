d39284bfc2693139b72164ca90575ea08e3afd95
Timothy James Shelton <tjbs2@kent.ac.uk>
2021-06-29 00:26:00 +0100

    Fix comment

--- gf-p2-256-32-977/SLDCC/source/p25632977mul.s
+++ gf-p2-256-32-977/SLDCC/source/p25632977mul.s
@@ -35,9 +35,9 @@
 | NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,| 
 | EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                          |
 +-----------------------------------------------------------------------------+
+q = n * p
+On call: %rdi = &q, %rsi = &n, %rdx = &p
 */
-; q = n * p
-; On call: %rdi = &q, %rsi = &n, %rdx = &p
 
 .p2align 5
 .globl gfp25632977mul
