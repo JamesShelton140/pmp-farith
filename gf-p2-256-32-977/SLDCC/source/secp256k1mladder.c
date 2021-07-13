e68f9c50223a74dc536e6ace9533d2f2fc07c5b8
Timothy Shelton <tjbs2@kent.ac.uk>
2021-07-13 13:27:01 +0100

    Add comments to mladder.c

--- gf-p2-256-32-977/SLDCC/source/secp256k1mladder.c
+++ gf-p2-256-32-977/SLDCC/source/secp256k1mladder.c
@@ -4,9 +4,15 @@
 
 void print_felem(const gfe_p25632977 *);
 
+/*
+* Elliptic curve scalar multiplication using basic Montgomery ladder
+* (Spelling of name will be corrected)
+*/
+
 // void secp256k1scalermult(gej_secp256k1 *nP, const gfe_p25632977 *n, const ge_secp256k1 *P) {
 //     gej_secp256k1 R0, R1;
 
+		// Set R0 = 0/inf, R1 = P
 //     R0 = (gej_secp256k1){0,1,0,1};
 //     R1.x = P->x;
 //     R1.y = P->y;
@@ -17,10 +23,10 @@ void print_felem(const gfe_p25632977 *);
 //         R1.z = (gfe_p25632977){1,0,0,0};
 //     }
     
-//     printf("\n\n\nstarting loop\n\n\n");
 //     int i, bit, limb;
 //     uint64 mask, swap;
 //     for (i = 255; i >= 0; i--) {
+			// select bit at position i
 //         limb = i/64;
 //         bit = i%64;
 //         mask = 1 << bit;
@@ -29,14 +35,19 @@ void print_felem(const gfe_p25632977 *);
 //         // gfp25632977readbit(&bit, n, limb);
 
 //         if (swap = 0) {
+				// R1 <- R0 + R1
+				// R0 <- 2R0
 //             secp256k1addjacobian(&R1, &R0, &R1);
 //             secp256k1doublejacobian(&R0, &R0);
 //         } else {
+				// R0 = R0 + R1
+				// R1 = 2R1
 //             secp256k1addjacobian(&R0, &R0, &R1);
 //             secp256k1doublejacobian(&R1, &R1);
 //         }
 //     }
     
+        // Set nP = R0
 //     nP->x = R0.x;
 //     nP->y = R0.y;
 //     nP->z = R0.z;
@@ -44,41 +55,42 @@ void print_felem(const gfe_p25632977 *);
 
 // }
 
-// Implement basic version of Joye ladder
+/*
+* Elliptic curve scalar multiplication using basic Joye ladder
+* (Spelling of name will be corrected)
+*/
+
 void secp256k1scalermult(gej_secp256k1 *nP, const gfe_p25632977 *n, const ge_secp256k1 *P) {
     gej_secp256k1 R0, R1, R_temp;
 
+	// Set R0, R1 = P
     R0 = (gej_secp256k1){P->x,P->y,1,P->infinity};
     R1 = (gej_secp256k1){P->x,P->y,1,P->infinity};
-    // R1.x = P->x;
-    // R1.y = P->y;
-    // R1.infinity = P->infinity;
-    // if (R1.infinity) {
-    //     R1.z = (gfe_p25632977){0,0,0,0};
-    // } else {
-    //     R1.z = (gfe_p25632977){1,0,0,0};
-    // }
     
     printf("\n\n\nstarting loop\n\n\n");
     int i, bit, limb;
     uint64 mask, swap;
     for (i = 1; i <= 255; i++) {
+    	// select bit at position i
         limb = i/64;
         bit = i%64;
         mask = 1 << bit;
-        swap = mask & n->l[limb];printf("%16llX ",swap);
+        swap = mask & n->l[limb];
         
         // gfp25632977readbit(&bit, n, limb);
 
         if (swap = 0) {
+        	// R1 <- 2R1 + R0
             secp256k1doublejacobian(&R_temp, &R1);
             secp256k1addjacobian(&R1, &R_temp, &R0);
         } else {
+        	// R0 <- 2R0 + R1
             secp256k1doublejacobian(&R_temp, &R0);
             secp256k1addjacobian(&R0, &R_temp, &R1);
         }
     }
     
+    // Set nP = R0
     nP->x = R0.x;
     nP->y = R0.y;
     nP->z = R0.z;
