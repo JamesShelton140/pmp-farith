1b29037a43d13d474316c44ef674a97be1411d6f
Timothy James Shelton <tjbs2@kent.ac.uk>
2021-07-22 18:12:14 +0100

    Change asm comments

--- gf-p2-256-32-977/SLDCC/source/p25632977readbit.s
+++ gf-p2-256-32-977/SLDCC/source/p25632977readbit.s
@@ -12,13 +12,13 @@ push    %r13
 push    %r14
 push    %r15
 
-movq    $8, %rax            // Move 8 into rax
-mulq    %rdx                // Multiply rax and rdx
-movq    (%rdi), %rcx        // Move the value of bit into rcx
-movq    $1, (%rdi)          // Move 1 into bit
-movq    (%rsi, %rax), %r8   // Move the rax-limb of n(at rsi) into r8
-shr     %cl, %r8            // Shift r8 right by cl bits
-andq    %r8, (%rdi)         // Set bit (at rdi) to r8 & 1.
+movq    $8, %rax            ;Move 8 into rax
+mulq    %rdx                ;Multiply rax and rdx
+movq    (%rdi), %rcx        ;Move the value of bit into rcx
+movq    $1, (%rdi)          ;Move 1 into bit
+movq    (%rsi, %rax), %r8   ;Move the rax-limb of n(at rsi) into r8
+shr     %cl, %r8            ;Shift r8 right by cl bits
+andq    %r8, (%rdi)         ;Set bit (at rdi) to r8 & 1.
 
 pop     %r15
 pop     %r14
